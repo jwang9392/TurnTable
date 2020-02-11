fda6d078d13bffc1bae87c72821e4302a911a874
jwang9392 <jwang9392@gmail.com>
2020-02-11 14:03:01 -0500

    Created and completed  basic file structuring for front end auth

---
+++ frontend/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const App = () => (
+  <div>
+    <header>
+      <h1>TurnTable</h1>
+    </header>
+
+  </div>
+);
+
+export default App;
--- frontend/turn_table.jsx
+++ frontend/turn_table.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {login, signup, logout} from './util/session_api_util';
+import configureStore from './store/store';
+import Root from './components/root'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<div>TurnTable?</div> , root)
+  const store = configureStore();
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
+  ReactDOM.render(<Root store={store}/> , root)
 })
