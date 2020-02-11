1a06998ea7809c1c97e74be001cffc34ecf10a54
jwang9392 <jwang9392@gmail.com>
2020-02-11 10:22:01 -0500

    Modified User and Sessions Controllers, and routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,7 @@
 class Api::UsersController < ApplicationController
   def show
-    @user = User.find_by_credentials(      
-      params[:user][:username],
-      params[:user][:password]
-    )
-
+    @user = User.find_by(id: params[:id])
+    
     render "api/users/show"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # namespace :api, defaults: {format: :json} do
-  #   resources :user, only: [:show, :create]
-  #   resource :session, only: [:create, :destroy]
-  # end
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:show, :create]
+    resource :session, only: [:create, :destroy]
+  end
 
   root "static_pages#root" 
 end
