9201f975d28813e1a641cccf359b292e5eccf42e
jwang9392 <jwang9392@gmail.com>
2020-02-20 04:06:50 -0500

    Fixed bug causing bootstrapped user to not persist

--- frontend/turn_table.jsx
+++ frontend/turn_table.jsx
@@ -7,7 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store;
   if (window.currentUser) {
-    const preloadedState = { session: { currentUserId: window.currentUser.id } };
+    const preloadedState = { 
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser },
+      },
+      session: { currentUserId: window.currentUser.id } 
+    };
     store = configureStore(preloadedState);
     delete window.currentUser;
   } else {
