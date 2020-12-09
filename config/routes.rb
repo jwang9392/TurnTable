432443df2b0ddd2c2200206f8133535faf264fd6
jwang9392 <jwang9392@gmail.com>
2020-12-09 13:06:08 -0500

    Adjusted routes to fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:show, :create] do
-      resources :reservations, only: [:index, :show, :update, :destroy]
+      resources :reservations, only: [:index]
     end
     resources :venues, only: [:index, :show] do
       resources :reservations, only: [:create]
     end 
-    # resources :reservations, only: [:show, :update, :destroy]
+    resources :reservations, only: [:show, :update, :destroy]
     resource :session, only: [:create, :destroy]
 
     get '/search', to: 'venues#search'
