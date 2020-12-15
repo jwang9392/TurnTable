cf615cef920b12a33e822517d5d6d105345241b9
jwang9392 <jwang9392@gmail.com>
2020-12-15 05:41:55 -0500

    Adjust reducers to clear state on logout

--- frontend/reducers/reservations_reducer.js
+++ frontend/reducers/reservations_reducer.js
@@ -3,6 +3,7 @@ import {
   RECEIVE_RESERVATIONS,
   REMOVE_RESERVATION,
 } from '../actions/reservation_actions';
+import { LOGOUT_CURRENT_USER } from '../actions/session_actions';
 
 const reservationsReducer = (state = {}, action) => {
   Object.freeze(state);
@@ -15,6 +16,8 @@ const reservationsReducer = (state = {}, action) => {
     case REMOVE_RESERVATION:
       delete newState[action.resId];
       return newState;
+    case LOGOUT_CURRENT_USER:
+      return Object.assign({});
     default:
       return state;
   }
