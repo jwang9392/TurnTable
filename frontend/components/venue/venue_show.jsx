5d389d4c069921369789b6eb1c02eb7b080b4640
jwang9392 <jwang9392@gmail.com>
2021-12-02 18:43:01 -0500

    Fix venue show page break on browser refresh

--- frontend/components/venue/venue_show.jsx
+++ frontend/components/venue/venue_show.jsx
@@ -3,6 +3,7 @@ import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import ReservationTimes from '../reservations/reservation_times_container';
 import { makeTimeOptions } from '../../util/util';
+import { fetchVenues } from '../../actions/venue_actions';
 
 class VenueShow extends React.Component {
   constructor(props) {
@@ -85,6 +86,12 @@ class VenueShow extends React.Component {
   }
 
   componentDidMount() {
+    const { venueId, venue, fetchVenue } = this.props;
+
+    if (!venue) {
+      fetchVenue(venueId);
+    }
+
     scrollTo(0, 0);
     window.addEventListener('scroll', this.handleScroll);
   }
@@ -103,119 +110,126 @@ class VenueShow extends React.Component {
   } 
 
   render() {
-    return (
-      <div>
-        <div id="splash" className="venue-splash">
-          {/* favorite button here pos abs */}
-        </div>
-        <div className="venue-info">
-          <div className="venue-info-columns">
-            <div className="venue-info-main">
-              <div className="venue-main-header">
-                <div className="venue-header-info">
-                  <input type="button" onClick={this.scroll} value="Overview" />
-                  <input type="button" onClick={this.scroll} value="Photos" />
-                  <input type="button" onClick={this.scroll} value="Reviews" />
-                </div>
-              </div>
-              <div className="venue-main-body">
-                <div className="spacer"></div>
-                <div id="Overview" className="venue-overview">
-                  <div className="venue-overview-name">
-                    <h2>{this.props.venue.name}</h2>
-                  </div>
-                  <div className="venue-overview-desc">
-                    <div>
-                      <span>Reviews</span>
-                    </div>
-                    <div>
-                      <i className="filter-icon fas fa-money-bill-alt"></i>
-                      <span>{this.props.venue.price}</span>
-                    </div>
-                    <div>
-                      <i className="filter-icon fas fa-headphones"></i>
-                      <span>{this.props.venue.genre}</span>
-                    </div>
+    const { venueId, venue, time } = this.props;
+
+    if (!venue) {
+      return <div></div>
+    } else {
+      return (
+        <div>
+          <div id="splash" className="venue-splash">
+            {/* favorite button here pos abs */}
+          </div>
+          <div className="venue-info">
+            <div className="venue-info-columns">
+              <div className="venue-info-main">
+                <div className="venue-main-header">
+                  <div className="venue-header-info">
+                    <input type="button" onClick={this.scroll} value="Overview" />
+                    <input type="button" onClick={this.scroll} value="Photos" />
+                    <input type="button" onClick={this.scroll} value="Reviews" />
                   </div>
                 </div>
-                <p id="Description">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                <div id="Photos">
-                  PHOTOS
-                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                </div>
-                <div id="Reviews">
-                  REVIEWS
-                </div>
-              </div>
-            </div>
-            <div className="venue-info-sidebar">
-              <div className="sidebar-header">
-                <h4>Make a reservation</h4>
-                <div className={this.state.scrolled ? "hidden" : "sidebar-long"}>
-                  <div>
-                    <div className="sidebar-selector sidebar-top">
-                      <div>Party Size</div>
+                <div className="venue-main-body">
+                  <div className="spacer"></div>
+                  <div id="Overview" className="venue-overview">
+                    <div className="venue-overview-name">
+                      <h2>{venue.name}</h2>
+                    </div>
+                    <div className="venue-overview-desc">
                       <div>
-                        <select id="party-selector" defaultValue={this.state.partySize}
-                          onChange={this.update("partySize")}>
-                          {this.partySizeOptions()}
-                        </select>
-                        <i className="fas fa-chevron-down"></i>
+                        <span>Reviews</span>
+                      </div>
+                      <div>
+                        <i className="filter-icon fas fa-money-bill-alt"></i>
+                        <span>{venue.price}</span>
+                      </div>
+                      <div>
+                        <i className="filter-icon fas fa-headphones"></i>
+                        <span>{venue.genre}</span>
                       </div>
                     </div>
-                    <div className="sidebar-bot">
-                      <div className="sidebar-selector">
-                        <div>Date</div>
+                  </div>
+                  <p id="Description">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                  <div id="Photos">
+                    PHOTOS
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                  </div>
+                  <div id="Reviews">
+                    REVIEWS
+                  </div>
+                </div>
+              </div>
+              <div className="venue-info-sidebar">
+                <div className="sidebar-header">
+                  <h4>Make a reservation</h4>
+                  <div className={this.state.scrolled ? "hidden" : "sidebar-long"}>
+                    <div>
+                      <div className="sidebar-selector sidebar-top">
+                        <div>Party Size</div>
                         <div>
-                          <DatePicker
-                            className="sidebar-datepicker datepicker"
-                            useWeekdaysShort={true}
-                            onFocus={e => e.target.blur()}
-                            dateFormat="MMM d, yyyy"
-                            selected={this.state.date}
-                            onChange={this.handleDateChange}
-                          />
-                          <i className="fas fa-chevron-down"></i>
-                        </div>
-                      </div>
-                      <div className="sidebar-selector">
-                        <div>Time</div>
-                        <div>
-                          <select id="time-selector" defaultValue={this.state.time === "" ? this.props.time : this.state.time}
-                            onChange={this.update("time")}>
-                            {makeTimeOptions()}
+                          <select id="party-selector" defaultValue={this.state.partySize}
+                            onChange={this.update("partySize")}>
+                            {this.partySizeOptions()}
                           </select>
                           <i className="fas fa-chevron-down"></i>
                         </div>
                       </div>
+                      <div className="sidebar-bot">
+                        <div className="sidebar-selector">
+                          <div>Date</div>
+                          <div>
+                            <DatePicker
+                              className="sidebar-datepicker datepicker"
+                              useWeekdaysShort={true}
+                              onFocus={e => e.target.blur()}
+                              dateFormat="MMM d, yyyy"
+                              selected={this.state.date}
+                              minDate={new Date()}
+                              onChange={this.handleDateChange}
+                            />
+                            <i className="fas fa-chevron-down"></i>
+                          </div>
+                        </div>
+                        <div className="sidebar-selector">
+                          <div>Time</div>
+                          <div>
+                            <select id="time-selector" defaultValue={this.state.time === "" ? time : this.state.time}
+                              onChange={this.update("time")}>
+                              {makeTimeOptions()}
+                            </select>
+                            <i className="fas fa-chevron-down"></i>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div onClick={this.handleClick} className={this.state.scrolled ? "sidebar-short" : "hidden"}>
-                  <div>For {this.state.partySize} people on {this.dateDisplay(this.state.date)}</div>
-                  <i className="fas fa-edit"></i>
-                </div>
-                <div className="sidebar-times">
-                  <span>Select a time:</span>
-                  <ReservationTimes 
-                    venueId={this.props.venueId} 
-                    date={this.state.date}
-                    time={this.state.time}
-                    partySize={this.state.partySize}
-                  />
+                  <div onClick={this.handleClick} className={this.state.scrolled ? "sidebar-short" : "hidden"}>
+                    <div>For {this.state.partySize} people on {this.dateDisplay(this.state.date)}</div>
+                    <i className="fas fa-edit"></i>
+                  </div>
+                  <div className="sidebar-times">
+                    <span>Select a time:</span>
+                    <ReservationTimes 
+                      venueId={venueId} 
+                      date={this.state.date}
+                      time={this.state.time}
+                      partySize={this.state.partySize}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <div className="spacer"></div>
         </div>
-        <div className="spacer"></div>
-      </div>
-    )
+      )
+    }
   }
 }
 
--- frontend/components/venue/venue_show_container.jsx
+++ frontend/components/venue/venue_show_container.jsx
@@ -1,17 +1,18 @@
 import { connect } from 'react-redux';
+import { fetchVenue } from '../../actions/venue_actions';
 import VenueShow from './venue_show';
 
 const mapStateToProps = (state, { match, location}) => {
   const venueId = match.params.id
   const venue = state.entities.venues[venueId]
+  let storedParams = JSON.parse(localStorage.getItem(`search-params`));
 
   return {
     venueId,
     venue, 
-    date: location.state.date,
-    time: location.state.time,
-    partySize: location.state.partySize,
-    timesBooked: location.state.timesBooked
+    date: location.state ? location.state.date : (storedParams ? new Date(storedParams.date) : new Date()),
+    time: location.state ? location.state.time : (storedParams ? storedParams.time : "9:00PM"),
+    partySize: location.state ? location.state.partySize : (storedParams ? storedParams.partySize : 2)
   }
 }
 
